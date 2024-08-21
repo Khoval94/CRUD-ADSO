4a80e2e3dc580315bfb7c93b5249be09fd1a1bf9
kristian.rueda94@gmail.com <>
2024-08-21 14:30:27 -0500

    Updated

---
+++ src/main/java/com/mycompany/crudadso/CrudAdso.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.crudadso;
+
+import com.mycompany.crudadso.igu.Principal;
+
+
+public class CrudAdso {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+}
